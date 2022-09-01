a7f7be1a7d8523fb9ded1ba6c60e4e51b87e5491
Moyses Azevedo <m.mirazev@gmail.com>
2022-09-01 09:58:03 -0300

    Initial Commit

---
+++ WorkoutProjectApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutProjectApp
+//
+//  Created by Moyses Miranda do Vale Azevedo on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
